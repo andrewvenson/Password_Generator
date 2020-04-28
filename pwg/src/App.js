d32ceff598ac971df1b87b85f17c9a2f3badade0
Andrew Venson <vensoneandrew@gmail.com>
2020-04-28 15:54:19 -0500

    fixed relative import

--- pwg/src/App.js
+++ pwg/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Prompt from "../Components/Prompt";
+import Prompt from "./Components/Prompt";
 
 function App() {
   return <div className="App"></div>;
