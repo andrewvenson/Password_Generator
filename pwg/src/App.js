e6adf9fad43150114292e5febb3d3bc81a704e98
Andrew Venson <vensoneandrew@gmail.com>
2020-04-28 21:52:04 -0500

    added all states

--- pwg/src/App.js
+++ pwg/src/App.js
@@ -3,6 +3,19 @@ import "./App.css";
 import Prompt from "./Components/Prompt";
 
 function App() {
+  // truth/falsy states
+  const [lowerCase, setLowercase] = useState(false);
+  const [upperCase, setUppercase] = useState(false);
+  const [numeric, setNumeric] = useState(false);
+  const [specialChars, setSpecialChars] = useState(false);
+
+  // numeric states
+  const [pwLength, setPwLength] = useState(0);
+  const [promptCount, setPromptCount] = useState(0);
+
+  // string states
+  const [generatedPW, setGenPw] = useState("");
+
   return <div className="App"></div>;
 }
 
