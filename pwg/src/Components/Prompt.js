dd4a009d7d03f62f3e6d6ca888cabea7af1d1e98
Andrew Venson <vensoneandrew@gmail.com>
2020-04-28 22:16:26 -0500

    added prompts array

--- pwg/src/Components/Prompt.js
+++ pwg/src/Components/Prompt.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Prompt = (props) => {
+  const prompts = [
+    "Choose length of password. Length must be between 8-128 characters:",
+    "Use lowercase characters:",
+    "Use uppercase characters:",
+    "Use numeric characters:",
+    "Use special characters:",
+  ];
   return (
     <div>
       <p>{props.prompt}</p>
