9038f9c420d7b22d7afab8e5d340a3c8eed4098f
Andrew Venson <vensoneandrew@gmail.com>
2020-04-30 11:57:17 -0500

    batched setValidationState within chained conditionals

--- src/Components/PromptModal.js
+++ src/Components/PromptModal.js
@@ -9,46 +9,66 @@ const PromptModal = (props) => {
   });
 
   const promptValidation = () => {
-    // if password length doesn't meet criteria set state to error message
+    // if both password length AND character type don't meet criteria
     if (
-      props.promptstate["length"] < 8 ||
-      props.promptstate["length"] > 128 ||
-      Number.isNaN(props.promptstate["length"])
-    ) {
-      setValidationState((validation) => ({
-        ...validation,
-        ["lengthValidation"]:
-          "Password length must be between 8-128 characters...",
-      }));
-    } else {
-      setValidationState((vlidation) => ({
-        ...validation,
-        ["lengthValidation"]: "",
-      }));
-    }
-
-    // if password characters aren't selected at all set state to error message
-    if (
+      (props.promptstate["length"] < 8 ||
+        props.promptstate["length"] > 128 ||
+        Number.isNaN(props.promptstate["length"])) &&
       !props.promptstate["specialCharacters"] &&
       !props.promptstate["upperCase"] &&
       !props.promptstate["lowerCase"] &&
       !props.promptstate["numbers"]
     ) {
-      setValidationState((validation) => ({
+      setValidationState({
         ...validation,
+        ["lengthValidation"]:
+          "Password length must be between 8-128 characters...",
         ["characterValidation"]:
           "At least one character type should be selected...",
-      }));
+      });
     }
-    // if password does meet require set state back to empty string
-    else {
-      setValidationState((prevValidation) => ({
-        ...prevValidation,
+    // if password length does NOT meet criteria and character type does meet criteria
+    else if (
+      (props.promptstate["length"] < 8 ||
+        props.promptstate["length"] > 128 ||
+        Number.isNaN(props.promptstate["length"])) &&
+      (props.promptstate["specialCharacters"] ||
+        props.promptstate["upperCase"] ||
+        props.promptstate["lowerCase"] ||
+        props.promptstate["numbers"])
+    ) {
+      setValidationState({
+        ...validation,
+        ["lengthValidation"]:
+          "Password length must be between 8-128 characters...",
         ["characterValidation"]: "",
-      }));
+      });
     }
-
-    // console.log(validation);
+    // if password length does meet criteria and character type does NOT meet criteria
+    else if (
+      (props.promptstate["length"] >= 8 ||
+        props.promptstate["length"] <= 128 ||
+        Number.isNaN(props.promptstate["length"])) &&
+      !props.promptstate["specialCharacters"] &&
+      !props.promptstate["upperCase"] &&
+      !props.promptstate["lowerCase"] &&
+      !props.promptstate["numbers"]
+    ) {
+      setValidationState({
+        ...validation,
+        ["lengthValidation"]: "",
+        ["characterValidation"]:
+          "At least one character type should be selected...",
+      });
+    }
+    // If both password length AND character type meet criteria
+    else {
+      setValidationState({
+        ...validation,
+        ["lengthValidation"]: "",
+        ["characterValidation"]: "",
+      });
+    }
   };
 
   return (
